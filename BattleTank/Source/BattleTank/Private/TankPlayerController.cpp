bc94df7b662c971e8aa6b253bfd97e39690742a2
Darkestt <martinez.alex.9790@gmail.com>
2019-10-19 12:21:39 -0400

    136. se crea base del metodo que realizara el raytrace, actualizando el Vector de Colision en cada Frame

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -21,13 +21,40 @@ void ATankPlayerController::Tick(float DeltaTime){
     Super::Tick(DeltaTime);
     //UE_LOG(LogTemp, Warning, TEXT("TankPlayerController Ticking."));
 
+    AimTowardsCrosshair();
 }
 
 ATank* ATankPlayerController::GetControlledTank() const{
-    return Cast<ATank>(GetPawn()); //retorna el Pawn que el cntrolador esta actualmente poseyendo y se castea como un Tanque
-    
+    return Cast<ATank>(GetPawn()); //retorna el Pawn que el controlador esta actualmente poseyendo y se castea como un Tanque
 }
 
 void ATankPlayerController::AimTowardsCrosshair(){
-    
+    //no tiene sentido apuntar a la pantalla si no tienes control sobre un tanque
+    if(!GetControlledTank()){
+        return;
+    }
+
+    FVector HitLocation;    //Out Parameter
+    //UE_LOG(LogTemp, Warning, TEXT("HitLocation = %s."), *HitLocation.ToString()); 
+
+    //se realiza un raytrace
+    if(GetSightRayHitLocation(HitLocation)){    
+        UE_LOG(LogTemp, Warning, TEXT("HitLocation = %s."), *HitLocation.ToString()); 
+    }
+
+    //Obtener la posicion mundo obtenido por el LineTrace a traves del Crosshair
+    //si intercepta el Landscape
+        //decirle al Tank que se controla que apunte en esta direccion
+}
+
+//Obtener la posicion mundo obtenido por el LineTrace a traves del Crosshair, true si coliciona con el Landscape
+bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const {
+
+    HitLocation = FVector(1.0);
+    return true;
+    /*if ()
+    {
+        return true;
+    }
+    return false;*/
 }
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -23,4 +23,6 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 		virtual void Tick(float DeltaTime) override;
 
 		void AimTowardsCrosshair();
+
+		bool GetSightRayHitLocation(FVector& HitLocation) const;
 };
