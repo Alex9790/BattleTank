c9c3c81f0edd781f043d01c55e38a95929904d7a
Darkestt <martinez.alex.9790@gmail.com>
2019-10-20 13:15:58 -0400

    141. Se crea nuevo componente para apuntar, se agrega a la clase Tank y  se delega la informacion AimAt al componente

--- BattleTank/Intermediate/Build/Win64/UE4Editor/Inc/BattleTank/BattleTank.init.gen.cpp
+++ BattleTank/Intermediate/Build/Win64/UE4Editor/Inc/BattleTank/BattleTank.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeBattleTank_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x72567DA9,
-				0xAD79653C,
+				0x62A4E802,
+				0x395DC0E9,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
